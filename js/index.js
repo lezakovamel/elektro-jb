1d27e97cd208a8ee79d87f31292e3fbfa997f360
Melánie <lezakova.melanie@gmail.com>
2020-06-01 17:03:20 +0200

    logo changes on resize

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ $(".gallery").magnificPopup({
 
 $(document).ready(function () {
   var nav = $("#nav");
-  var logoColors = $("colors-logo");
-  var logoWhite = $("white-logo");
+  var logoColors = $("#colors-logo");
+  var logoWhite = $("#white-logo");
   $(window).scroll(function () {
     var ScrollTop = parseInt($(window).scrollTop());
     console.log(ScrollTop);
@@ -24,11 +24,18 @@ $(document).ready(function () {
       logoColors
         .removeClass("__not-displayed-logo")
         .addClass("__displayed-logo");
+      logoWhite
+        .removeClass("__displayed-logo")
+        .addClass("__not-displayed-logo");
     } else if (ScrollTop < 200) {
       nav.removeClass("__white").addClass("__none").fadeIn;
       logoWhite
         .removeClass("__not-displayed-logo")
         .addClass("__displayed-logo");
+      logoColors
+
+        .removeClass("__displayed-logo")
+        .addClass("__not-displayed-logo");
     }
   });
 });
