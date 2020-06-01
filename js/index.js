a71b078f1519727d2ef9f354320cd80f96e520a7
Melánie <lezakova.melanie@gmail.com>
2020-06-01 16:43:34 +0200

    navigation resize and color change on scroll

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
 $(".carousel").carousel({
-  interval: 2000
+  interval: 2000,
+});
+
+$(".gallery").magnificPopup({
+  delegate: "a",
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
+
+$(document).ready(function () {
+  var nav = $("#nav");
+  var logoColors = $("colors-logo");
+  var logoWhite = $("white-logo");
+  $(window).scroll(function () {
+    var ScrollTop = parseInt($(window).scrollTop());
+    console.log(ScrollTop);
+
+    if (ScrollTop > 200) {
+      console.log("tu jsem");
+      nav.removeClass("__none").addClass("__white").fadeIn;
+      logoColors
+        .removeClass("__not-displayed-logo")
+        .addClass("__displayed-logo");
+    } else if (ScrollTop < 200) {
+      nav.removeClass("__white").addClass("__none").fadeIn;
+      logoWhite
+        .removeClass("__not-displayed-logo")
+        .addClass("__displayed-logo");
+    }
+  });
 });
